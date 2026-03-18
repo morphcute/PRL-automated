39db9033453091728ec949a95f5e0ae1cb36ca0a
morphcute <evangelistakimlester@gmail.com>
2026-03-18 12:03:39 +0800

    add database performance indexes

---
+++ prisma/migrations/20260318040258_init_indexes/migration.sql
@@ -0,0 +1,17 @@
+-- DropIndex
+DROP INDEX "SyncJob_endAt_idx";
+
+-- DropIndex
+DROP INDEX "SyncJob_lastRunAt_idx";
+
+-- DropIndex
+DROP INDEX "SyncJob_name_userId_idx";
+
+-- DropIndex
+DROP INDEX "SyncJob_startAt_idx";
+
+-- DropIndex
+DROP INDEX "SyncRun_jobId_idx";
+
+-- DropIndex
+DROP INDEX "SyncRun_status_startedAt_idx";
